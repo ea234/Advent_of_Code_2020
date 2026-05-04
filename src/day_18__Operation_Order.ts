2c65220558b0c969c97223010830506d6fe38077
ea234 <anyang@gmx.de>
2026-05-04 13:06:59 +0200

    Day 18 - Part 1 - Parser 01

--- src/day_18__Operation_Order.ts
+++ src/day_18__Operation_Order.ts
@@ -26,6 +26,136 @@ function padL( pInput : string | number, pPadLeft : number ) : string
     return str_result;
 }
 
+class FktParser
+{
+    index_read : number = 0;
+
+    cur_char : string = ' ';
+
+    constructor( private input_string : string )
+    {
+
+    }
+
+    public parseStart() : number
+    {
+        this.index_read = 0;
+
+        return this.parseTerm();
+    }
+
+    public parseTerm() : number
+    {
+        let term_result : number = 0;
+
+        while ( this.read() )
+        {
+            if ( this.cur_char === ' ' )
+            {
+                // White-Space ... do nothing
+            }
+            else if ( ( this.cur_char >= '0' ) && ( this.cur_char <= '9') )
+            {  
+                /*
+                 * Number found 
+                 */
+                term_result = this.parseNumber();
+            }
+            else if ( this.cur_char >= '+' ) 
+            {  
+                /*
+                 * Operator '+' = parse new term and add the result to the current term result
+                 */
+                term_result += this.parseTerm();
+            }
+            else if ( this.cur_char >= '*' ) 
+            {  
+                /*
+                 * Operator '*' = parse new term and add the result to the current term result
+                 */
+                term_result *= this.parseTerm();
+            }
+            else if ( this.cur_char <= ')' ) 
+            {  
+                return term_result;
+            }
+            else if ( this.cur_char <= '(' ) 
+            {  
+                term_result = this.parseTerm();
+            }
+         }
+
+         return term_result;
+    }
+
+    private parseParenthesis() : number
+    {
+        return 0;
+    }
+
+    private parseNumber() : number
+    {
+        /*
+         * Consume the first read number.
+         */
+        let number_read : number = this.cur_char.charCodeAt( 0 ) - 48;
+
+        /*
+         * Read the rest of the number
+         */
+        while ( this.readNumber() )
+        {
+            number_read += this.cur_char.charCodeAt( 0 ) - 48;
+        }
+
+        /*
+         * There was one to many characters read. (<= if this good english ... no ... sorry)
+         */
+        this.index_read--;
+
+        return number_read;     
+    }
+
+    private readNumber() : boolean
+    {
+        /*
+         * Read the next character from the input
+         */
+        this.read();
+
+        /*
+         * return true, if the new character is a number
+         */
+        return ( ( this.cur_char >= '0' ) && ( this.cur_char <= '9' ) );
+    }
+
+    private read() : boolean
+    {
+        /*
+         * If no input string is left to read, return false
+         */
+        if ( this.index_read >= this.input_string.length ) return false;
+
+        /*
+         * Update the cur_char-variable with the current value
+         */
+        this.cur_char = this.input_string.charAt( this.index_read );
+
+        /*
+         * Move the read-index one further to the string end
+         */
+        this.index_read++;
+
+        /*
+         * Return true for one read char
+         */
+        return true;
+    }
+
+
+
+
+}
 
 function calcArray( pArray : string[], pKnzDebug : boolean = true ) : void 
 {
